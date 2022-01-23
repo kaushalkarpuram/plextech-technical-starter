ca465e808f16c22344f711e041e8ba6299a072e0
Akshat Jain <akshatj@berkeley.edu>
2022-01-22 21:09:17 -0800

    added more scaffolding for controller functions

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # PlexTech Take-Home Technical Interview
 Starter Code for the take-home portion of the PlexTech Technical Interview
 
+API Spec: https://app.swaggerhub.com/apis-docs/AkshatJain1/Petstore
+
 # Server Installation Instructions
 1. Make sure up-to-date versions of Node and NPM are installed.
 2. Run `npm install` in project directory 
@@ -10,3 +12,9 @@ Starter Code for the take-home portion of the PlexTech Technical Interview
 1. Make sure at least Python 3.7 is installed
 2. Run `pip install -r requirements.txt` in the `test` folder
 3. Run `npm test` from the base folder. Starter test should succeed if the server is running
+
+# Recommended Workflow
+1. Define Pet Object Schema in `src/database/pets.js`
+2. Add more test data in `src/database/pets.js`
+3. Complete the controller functions in `src/database/pets.js`
+4. Specify route definitions in `src/index.js` using the controller functions
---
+++ src/database/pets.js
@@ -0,0 +1,92 @@
+const Ajv = require("ajv/dist/jtd")
+const ajv = new Ajv()
+
+
+const pet_schema = {
+    // TODO: Complete schema
+        // - Utilize Pet Schema in spec
+        // - Use Ajv JSON Type Definition: https://ajv.js.org/json-type-definition.html
+};
+
+const isValid = ajv.compile(pet_schema);
+
+// TODO: Add more test data to work with
+    // - Test data must be an array of 'Pet' objects
+    // - Private data (should NOT be exported)
+    // - Note that since we're storing our data in-memory (not persistent),
+    //    this data will reset everytime the program restarts
+const data = [
+    {
+        "id": 0,
+        "name": "doggie",
+        "photoUrls": [
+            "http://google.com/"
+        ],
+        "tags": [
+            "tag1"
+        ],
+        "status": "available"
+    },
+    {
+        "id": 3,
+        "name": "catapalooza",
+        "photoUrls": [
+            "https://www.yahoo.com/"
+        ],
+        "tags": [],
+        "status": "sold"
+    }
+];
+
+
+/* CONTROLLER FUNCTIONS */
+
+/* Adds a new Pet object to 'data'. Validates input data
+    and returns true if successful, otherwise false. */
+exports.createPet = function (pet) {
+    // checks 'pet' according to Ajv schema you defined above
+    if (!isValid(pet)) {
+        return false;
+    }
+    // TODO: Consider edge case
+
+    data.push(pet);
+    return true;
+};
+
+/* Returns Pet object as specified by 'id' */
+exports.getPetById = function (id) {
+    const pet = data.find(
+        element => element.id === id
+    );
+    // TODO: Consider edge case
+
+    return pet;
+};
+
+// TODO: complete filter functions
+exports.getPetsByStatus = null;
+exports.getPetsByTags = null;
+
+/* Executes a full replace with the input pet data. Returns true 
+    if successful, otherwise false  */
+exports.updatePetById = function (pet) {
+    // checks 'pet' according to Ajv schema you defined above
+    if (!isValid(pet)) {
+        return false;
+    }
+
+    // TODO: execute a full replace based on pet.id
+}
+
+/* Deletes Pet object as specified by 'id'. Returns true 
+    if successful, otherwise false  */
+exports.deletePet = function (id) {
+    const petIndex = data.findIndex(
+        element => element.id === id
+    );
+    // TODO: Consider edge case
+
+    data.splice(petIndex, 1);
+    return true;
+};
